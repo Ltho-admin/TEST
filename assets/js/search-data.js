7b2ee0a0b14046f1902e5b1fc343e3cd7115a80e
Ltho-admin <vagascience@gmail.com>
2025-10-15 09:41:42 +0000

    Deploying to gh-pages from @ Ltho-admin/TEST@b60d96a70f851815c39793b88ed59429b0e21c94 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,8 +420,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/TEST/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-premier-test-de-mise-en-ligne-du-site-sparkles-nerd-face",
+          title: 'Premier test de mise en ligne du site ! :sparkles::nerd_face:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
